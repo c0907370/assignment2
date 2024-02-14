b16a6fd84763c2a0ee4a396bdbc528dc798c5bdd
c0907370 <160021676+c0907370@users.noreply.github.com>
2024-02-14 18:13:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Interval
+{
+    static void Main()
+    {
+        Console.Write("Enter a real number: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        if ((x >= 2 && x < 3) || (x > 0 && x <= 1) || (x >= -10 && x <= -2))
+        {
+            Console.WriteLine("x belongs to I");
+        }
+        else
+        {
+            Console.WriteLine("x does not belong to I");
+        }
+    }
+}
